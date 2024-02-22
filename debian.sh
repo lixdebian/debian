d0975383b583fc00229f738def321a2d4dc88627
deblix <60364250+lixdebian@users.noreply.github.com>
2024-02-22 11:31:42 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,1 +1,56 @@
+#!/usr/bin/env bash
+#
+# debian.sh install,funciona debian 11 e 12 sem nenhuma problema !
 
+# como executar o programa chmod 777 debian.sh
+su -i
+apt update & apt dist-upgrade -y
+# programas 
+##URLS
+URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+URL_STEAM="https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb"
+#pasta downloads
+
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+FILE="/home/$USER/.config/gtk-3.0/bookmarks"
+
+# instalação pros programas
+install_deb() {
+
+# remover travas futuras do apt
+travas_apt(){
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+# adicionar arquitetura de 32 bits
+add_archi386(){
+sudo dpkg --add-architecture i386
+# atualizar o rep
+apt update & apt dist-upgrade -y
+# debs programas principais
+flatpak
+virtualbox
+gpaterd
+timeshitf
+vlcflatpak
+gnome-disk-utility
+baobab
+git
+wget
+)
+
+#instalação downloads acima
+install_debs(){
+
+echo -e "${VERDE}[INFO] - Baixando pacotes .deb${SEM_COR}"
+mkdir "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_STEAM"               -P "DIRETORIO_DOWNLOADS"
+
+# instalando pacotes .deb baixados na sessão anterior #
+sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
+# instalando via apt
+apt install vlc emarcs inkscape krita gimp qimgv qbittorrent goverlay gpaterd gnome-disk-utility timeshitf flatpak gnome-software-plugin-flatpak
+# repositorio do flathub
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#da uma reiniciada no sistema 
+reboot
